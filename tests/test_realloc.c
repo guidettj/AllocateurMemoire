f905f6297aa2dba9b04fa1ff07ed7aa02159bfa0
guidettj <jorane.guidetti@etu.univ-grenoble-alpes.fr>
2023-10-11 12:15:51 +0200

    update test_realloc

--- tests/test_realloc.c
+++ tests/test_realloc.c
@@ -14,26 +14,24 @@
 int main() {
     mem_init();
  
-    struct bb *a = mem_alloc(10);
-    struct bb *b = mem_realloc(a, 8) - sizeof(struct bb);
-    // long int * bb = (long int *) b;
+    void *a = mem_alloc(10);
+    struct bb *b = mem_realloc(a, 8) - sizeof(size_t);
+
     assert(b != NULL);
     assert(b->size == 8);
 
     mem_init();
-    struct bb *c = mem_alloc(10);
+    void *c = mem_alloc(10);
     struct bb *d = mem_realloc(c, 18) - sizeof(struct bb);
     assert(d != NULL);
     assert(d->size == 18);
 
     mem_init();
-    struct bb *e = mem_alloc(10);
-    struct bb *f = mem_realloc(e, 0) - sizeof(struct bb);
-    printf("%ld\n",b->size);
-    assert(f->size == 8);  
+    void *e = mem_alloc(10);
+    struct bb *bb = (struct bb *)((char *)e - sizeof(size_t));
+    mem_realloc(e, 0);
+    assert(bb->size != 0);
 
-    mem_init();
-    a = mem_alloc(10);
-
+ 
     printf("Test de mem_realloc réussi !\n");
 }
